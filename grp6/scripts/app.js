a404dc563c13c2254740136e50bd1523f93094ff
mathys_craft <41139317+mathyscraft@users.noreply.github.com>
2022-04-10 07:24:11 +0200

    Experiment/scroll animation (#1)
    * UTF-8
    
    * hello world
    
    * animation au scroll schema balisage

---
+++ grp6/scripts/app.js
@@ -0,0 +1,15 @@
+// Animation au scroll balisage
+const sliding = document.querySelector("#img-balisage"); //Selectione la ligne de code avec l'id "img-balisage"
+
+window.addEventListener("scroll", () => {
+    
+    const screenHeight = document.documentElement.clientHeight; //La valeur de doc.docElmt.clientHeight = taille de l'écran
+
+    const topElementToTopScreen = sliding.getBoundingClientRect().top; //Cette valeur correspond à la distance du haut de l'élement par rapport à l'écran
+
+    if (scrollY > (scrollY + topElementToTopScreen) - screenHeight * 0.8) {
+        sliding.classList.add("active");
+        document.querySelector("#text-balisage").classList.add("active");
+    }
+    
+})
