50a3eb2e8e96d7825261636da1ed80a68347d9d2
mathys_craft <41139317+mathyscraft@users.noreply.github.com>
2022-04-20 18:35:04 +0200

    page HTML finie !!!

--- grp6/scripts/quotes.js
+++ grp6/scripts/quotes.js
@@ -1,16 +1,13 @@
-
 const quote1 = document.getElementById("quote1");
 quote1.hidden = false;
 
 const quote2 = document.getElementById("quote2");
 quote2.hidden = true;
 
-document.getElementById("switch-btn").onclick = function() {
-    quote1.hidden = !quote1.hidden
-    quote2.hidden = !quote2.hidden
-}
+document.getElementById("switch-btn").onclick = quoteSwitch;
+document.getElementById("switch-btn2").onclick = quoteSwitch;
 
-document.getElementById("switch-btn2").onclick = function() {
-    quote1.hidden = !quote1.hidden
-    quote2.hidden = !quote2.hidden
+function quoteSwitch() {
+    quote1.hidden = !quote1.hidden;
+    quote2.hidden = !quote2.hidden;
 }
